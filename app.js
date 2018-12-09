4da58775a7385a6d673a0800c67de5eb7fea96fc
Vladimir Lyashinsky <Vladimir.Lyashinksy@artezio.com>
2018-12-09 04:24:26 +0300

    some fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http'),
     express = require('express'),
     brain = require('brain.js'),
     bodyParser = require("body-parser"),
-    path = require("path");
+    path = require("path"),
     app = express();
 
 const urlencodedParser = bodyParser.urlencoded({extended: false});
@@ -12,40 +12,54 @@ const parserJSON = bodyParser.json();
 const game = require("./model/game");
 const net = new brain.NeuralNetwork();
 
+let nowGameID;
+
 let trainArr = [];
-let filesTrain;
+
+let trainInFile = [];
+let trainInMLab;
+
+fs.readdir(path.join(__dirname, "TRAIN_PROGRAM"), { encoding: "UTF-8" } , (err, files) => {
+    if (err) console.log(err);
+    console.log("*Считывание файлов JSON!");
+    files.forEach((file) => {
+        if (String(file).match("^([A-Za-z0-9]+).json$")) {
+            let data = fs.readFileSync(path.join(__dirname, "TRAIN_PROGRAM", file))
+            trainInFile.push(...(JSON.parse(data)));
+        }
+    });
+});
+
 /*
-    0 0  -  ""  -  0
-    0 1  -  1   -  1
-    1 0  -  0   -  0.5
+    0   -  ""    -  0
+    0.5 -  "0"   -  0.5
+    1   -  "X"   -  1
 */
 app.get("/", (req, res) => {
-    fs.readdir(path.join(__dirname, "TRAIN_PROGRAM"), { encoding: "UTF-8" } , (err, files) => {
+    trainInMLab = [];
+
+    game.find({}).exec((err, docs) => {
         if (err) console.log(err);
-        files.forEach((file) => {
-            if (String(file).match("^([A-Za-z0-9]+).json$")) {
-                let data = fs.readFileSync(path.join(__dirname, "TRAIN_PROGRAM", file))
-                trainArr.push(...(JSON.parse(data)));
+
+        console.log("*Считывание предыдущих игр из mLab!");
+
+        docs.forEach((value, index) => {
+            if (value.Train != []) {
+                trainInMLab.push(...value.Train);
             }
         });
-        filesTrain = trainArr.length;
-        game.find({}, (err, docs) => {
-            if (err) console.log(err);
-            docs.forEach((value, index) => {
-                trainArr.push(...value.Train);
-            });
-            fs.readFile(__dirname + "/dist/index.html", (err, data) => {
-                res.writeHead(200, {"Content-type": "text/html; charset=utf-8"})
-                res.end(data);
-            });
+
+        console.log("*Ожидание USER_NAME и IS_X!"); 
+        fs.readFile(__dirname + "/dist/index.html", (err, data) => {
+            res.writeHead(200, {"Content-type": "text/html; charset=utf-8"})
+            res.end(data);
         });
-    }); 
+    });
 });
 
 app.post("/ticitac", parserJSON, (req, res) => {
     let AllGames;
     let BotWins;
-    let TrainData = 0;
 
     if (!req.body) return response.sendStatus(400);
     if (typeof(req.body.UserName) != "undefined" && typeof(req.body.Side) != "undefined"){
@@ -54,31 +68,28 @@ app.post("/ticitac", parserJSON, (req, res) => {
             if (err) console.log(err);
             AllGames = docs.length;
             console.log("***Получение информации о количестве игр выигравших ботом!");
-            game.find({result: 0}, (err, docs)  => {
+            game.find({ Result: 1 }).exec((err, docs)  => {
+                
                 if (err) console.log(err);
                 BotWins = docs.length;
                 console.log("***Получение информации о количестве тренеруемых данных!");
-                game.find({}, (err, docs) => {
-                    if (err) console.log(err);
-                    docs.forEach((value, index) => {
-                        TrainData += value.Train.length;
-                    })
-                    res.json({
-                        UserName: req.body.UserName,
-                        AllGames: AllGames,
-                        BotWins: BotWins,
-                        TrainData: TrainData + filesTrain,
-                        FirstStep: Math.round(Math.random())
-                    });
-                    game.create({
-                        UserName: req.body.UserName,
-                        Result: "-1",
-                        Train: []
-                    }).then((game) => {
-                        console.log(`Document ${game._id} created`);
-                    });
-                })
-            })  
+
+                res.json({
+                    UserName: req.body.UserName,
+                    AllGames: AllGames,
+                    BotWins: BotWins,
+                    TrainData: (trainInFile.length + trainInMLab.length),
+                    FirstStep: Math.round(Math.random())
+                });
+                game.create({
+                    UserName: req.body.UserName,
+                    Result: "-1",
+                    Train: []
+                }).then((game) => {
+                    nowGameID = game._id;
+                    console.log(`Document ${game._id} created`);
+                });
+            });
         });
     }
     else {
@@ -89,21 +100,39 @@ app.post("/ticitac", parserJSON, (req, res) => {
 
 app.get("/tictac/train", (req, res) => {
     console.log("**Начало обучения!");
-    net.train(trainArr, {
+    console.log("Количество тренеруемых данных: " + (trainInFile.length + trainInMLab.length));
+
+    net.train(new Array(...trainInFile, ...trainInMLab), {
         log: true
     });
+
     console.log("**Конец обучения!");
     res.sendStatus(200);
 });
 
 app.post("/tictac/step", parserJSON, (req, res) => {
     console.log("*Начало хода!");
+    if (req.body.Train.output != undefined) {
+        game.updateOne({ _id: nowGameID }, { $push: { Train: req.body.Train } })
+            .then(() => {
+                console.log("*Произошла запись обучащей информации!");
+            });
+    }
     let result = net.run(req.body.TableForStep);
     res.json({
         res: result
     });
     console.log(result);
     console.log("*Конец хода!");
+});
+
+app.post("/tictac/end", parserJSON, (req, res) => {
+    console.log("*Конец игры!");
+    game.updateOne({ _id: nowGameID }, { Result: req.body.Result })
+        .then(() => {
+            console.log("*Произошла запись об окончании игры!");
+        });
+    
 });
 
 app.get("/css/style.css", (req, res) => {
--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,17 +1,28 @@
 $(document).ready(function(){
-    let isX; //крестик или нолик
-    let UserName;
+    let IS_X; //крестик или нолик игрока
+    let USER_NAME;
+    const SIZE_TABLE = 10;
+    const SEQUENCE_WIN = 5;
+    const VALUE_X = 1;
+    const VALUE_0 = 0.5;
+    const VALUE_EMPTY = 0;
+
+    const BOT_WON = 1;
+    const PLAYER_WON = 2;
 
     //Функции последовательного отображения модальных окон при первоначальном запуске
     function startGame(){
         $(".wrap-overlay-choice").removeClass("filter-blur5");
+
         $(".wrap-overlay-loading").css({
             display: "none"
         });
+
         $(".wrap-overlay-choice-window-x").click(function(e){
             $(this).addClass("wrap-overlay-choice-window-x-active");
             $(".wrap-overlay-choice-window-0").removeClass("wrap-overlay-choice-window-0-active");
         });
+
         $(".wrap-overlay-choice-window-0").click(function(e){
             $(this).addClass("wrap-overlay-choice-window-0-active");
             $(".wrap-overlay-choice-window-x").removeClass("wrap-overlay-choice-window-x-active");
@@ -54,17 +65,25 @@ $(document).ready(function(){
         removeMainBlur5();
     }
 
+    function unbindClick() {
+        $(".wrap-playing-field-table tr td").each(function (index, val) {
+            if (index % (SIZE_TABLE + 1) != 0) {
+                $(val).unbind("click");
+            }
+        });
+    }
+
     /**
      * При вызове функции формируется массив, на основе игрового поля который содержить 
      * в себе коэффициенты от 0 до 1. 
      *      Если в ячейке ничего не стоить, то будет записано 0.
      *      Если в ячейке стоит 0, то будет записано 0.5.
-     *      Если в ячейке стоит 1, то будет записано 1.
+     *      Если в ячейке стоит X, то будет записано 1.
      */
     function getArrayCoefficients() {
         let arr = [];
         $(".wrap-playing-field-table tr td").each(function (index, val) {
-            if (index % 11 != 0) {
+            if (index % (SIZE_TABLE + 1) != 0) {
                 if ($(val).html() == "") arr.push(0);
                 if ($(val).html() == "0") arr.push(0.5);
                 if ($(val).html() == "X") arr.push(1)
@@ -80,14 +99,17 @@ $(document).ready(function(){
      * 
      *  Функци возвращает объект необходимый для отправки запроса 
      */
-    function getSendObject(arr, index) {
-        let rightCoord = Math.floor(index / 11);
-        let topCoord = (index - rightCoord) - (rightCoord * 10) - 1;
-        console.log(rightCoord + " - " + topCoord);
-        return ({
+    function getTrainData(arr, index) {
+        let rightCoord = Math.floor(index / (SIZE_TABLE + 1));
+        let topCoord = (index - rightCoord) - (rightCoord * SIZE_TABLE) - 1;
+
+        let result = {
             input: arr,
-            output: [topCoord, rightCoord]
-        });
+            output: { }
+        }
+        let temp = `${rightCoord}${topCoord}`;
+        result.output[temp] = 1;
+        return result;
     }
     /**
      * 
@@ -96,7 +118,7 @@ $(document).ready(function(){
      */
     function isEmptyCell(index){
         let arr = getArrayCoefficients();
-        let i = Math.floor(index / 10) * 10 + (index % 10);
+        let i = Math.floor(index / SIZE_TABLE) * SIZE_TABLE + (index % SIZE_TABLE);
         if (arr[i] == 0) {
             return true;
         } else {
@@ -106,15 +128,16 @@ $(document).ready(function(){
 
     startGame();
 
+    //НАЧАЛО
     $(".wrap-overlay-choice-window-arr").click(function(e){
-        UserName = $(".wrap-overlay-choice-window-input").val();
-        isX = $(".wrap-overlay-choice-window-x").hasClass("wrap-overlay-choice-window-x-active");
+        USER_NAME = $(".wrap-overlay-choice-window-input").val();
+        IS_X = $(".wrap-overlay-choice-window-x").hasClass("wrap-overlay-choice-window-x-active");
         
-        if (UserName != ""){
+        if (USER_NAME != ""){
             $.ajax({
                 type: "POST",
                 url: "/ticitac",
-                data: JSON.stringify({ UserName: UserName, Side: isX }),
+                data: JSON.stringify({ UserName: USER_NAME, Side: IS_X }),
                 dataType: "json",
                 contentType: "application/json",
                 beforeSend: function() {
@@ -123,8 +146,8 @@ $(document).ready(function(){
                 },
                 success: function (response) {
                     //Заполнение блока статистики
-                    $(".wrap-info-stats-block div:nth-child(1) span").html(response.UserName).css({
-                        color: (!isX) ? ("#e41d23") : ("#2c73b8")
+                    $(".wrap-info-stats-block div:nth-child(1) span").html(response.USER_NAME).css({
+                        color: (!IS_X) ? ("#e41d23") : ("#2c73b8")
                     });
                     $(".wrap-info-stats-block div:nth-child(2) span").html(response.AllGames);
                     $(".wrap-info-stats-block div:nth-child(3) span").html(response.BotWins);
@@ -140,7 +163,7 @@ $(document).ready(function(){
                             showLoading("System training...");
                         },
                         success: function(response) {
-                            if (isX != whoFirst){
+                            if (IS_X != whoFirst){
                                 makeMove({}, getArrayCoefficients());
                             }
                             else{
@@ -186,8 +209,10 @@ $(document).ready(function(){
                     }
                     if (isEmptyCell(arrCoord[maxIndex])) {
                         $(".wrap-playing-field-table tr td").each(function (index, val) {
-                            if (index == (Math.floor(arrCoord[maxIndex] / 10) * 11 + (arrCoord[maxIndex] % 10) + 1)) {
-                                $(this).html((!isX) ? ("X") : ("0"));
+                            if (index == (Math.floor(arrCoord[maxIndex] / SIZE_TABLE) * (SIZE_TABLE + 1) + 
+                                    (arrCoord[maxIndex] % SIZE_TABLE) + 1)) {
+                                $(this).unbind("click");
+                                $(this).html((!IS_X) ? ("X") : ("0"));
                             }
                         });
                         break;
@@ -199,87 +224,303 @@ $(document).ready(function(){
                 if (arrCoord.length == 0) {
                     $('.wrap-playing-field-table caption').html("A small problem! Due to the lack of trained data, " +
                             "the neural network does not know where to go! Please restart the game.");
-                    $(".wrap-playing-field-table tr td").each(function (index, val) {
-                        if (index % 11 != 0) {
-                            $(val).unbind("click");
-                        }
-                    });
+                    unbindClick();
+                } else {
+                    let result = isContainSequnceInRow();
+                    if (result != null) {
+                        endGame(result);
+                    }
                 }
+
                 closeLoading();
             }
         });
     }
 
+    function endGame(result) {
+        unbindClick();
+        $('.wrap-playing-field-table caption').html(`End game.
+                Won ${(result[0].value == VALUE_X) ? ("X") : ("0")} - 
+                ${((result[0].value == VALUE_X) == IS_X) ? ("player " + USER_NAME) : ("BOT")}
+                . Please start the game again.`);
+        $(".wrap-playing-field-table tr td").each(function (index, val) {
+            let rightCoord = Math.floor(index / (SIZE_TABLE + 1));
+            let topCoord = (index - rightCoord) - (rightCoord * SIZE_TABLE) - 1;
+            result.forEach(element => { 
+                if (element.coord[0] == rightCoord && topCoord == element.coord[1]) {
+                    $(this).css({
+                        background: "#e41d23"
+                    });
+                }
+            });
+        });
+        $.ajax({
+            type: "POST",
+            url: "/tictac/end",
+            data: JSON.stringify({ 
+                Result: (result[0].value != IS_X) ? (PLAYER_WON) : (BOT_WON),
+            }),
+            dataType: "json",
+            contentType: "application/json",
+            success: function (response) {
 
-    // Функция проверки есть на оле 5 в ряд либо X, либо 0
-    function isContainFiveInRow() {
+            }
+        })
+    }
+
+    // Функция определения содержат ли линейные массивы SEQUENCE_WIN в ряд
+    function isContainSequenceLineir(arr1, arr2) {
+
+        let sequenceArr1X = [];
+        let sequenceArr10 = [];
+
+        let sequenceArr2X = [];
+        let sequenceArr20 = [];
+        
+        let temp1;
+        let temp2;
+
+        for (let row = 0; row < SIZE_TABLE; row++) {
+            for (let col = 0; col < SIZE_TABLE; col++) {
+
+                temp1 = arr1[row][col];
+                temp2 = arr2[row][col];
+
+                if (temp1.value == VALUE_0) {
+                    sequenceArr10.push(arr1[row][col]);
+                    sequenceArr1X = [];
+                } else if (temp1.value == VALUE_X) {
+                    sequenceArr1X.push(arr1[row][col]);
+                    sequenceArr10 = [];
+                } else {
+                    sequenceArr1X = [];
+                    sequenceArr10 = [];
+                }
+
+                if (temp2.value == VALUE_0) {
+                    sequenceArr20.push(arr2[row][col]);
+                    sequenceArr2X = [];
+                } else if (temp2.value == VALUE_X) {
+                    sequenceArr2X.push(arr2[row][col]);
+                    sequenceArr20 = [];
+                } else {
+                    sequenceArr2X = [];
+                    sequenceArr20 = [];
+                }
+
+                if (sequenceArr10.length == SEQUENCE_WIN) {
+                    return sequenceArr10;
+                }
+                if (sequenceArr1X.length == SEQUENCE_WIN) {
+                    return sequenceArr1X;
+                }
+                if (sequenceArr20.length == SEQUENCE_WIN) {
+                    return sequenceArr20;
+                }
+                if (sequenceArr2X.length == SEQUENCE_WIN) {
+                    return sequenceArr2X;
+                }
+            }
+            
+            sequenceArr1X = [];
+            sequenceArr10 = [];
+            sequenceArr2X = [];
+            sequenceArr20 = [];
+        }
+        return null;
+    }
+
+    //Функция определения содержат ли диагональные массивы SEQUENCE_WIN в ряд
+    function isContainSequnceDiagonal(arr3, arr4) {
+
+        let sequenceArr3X = [];
+        let sequenceArr30 = [];
+
+        let sequenceArr4X = [];
+        let sequenceArr40 = [];
+
+        let temp3;
+        let temp4;
+
+        for (let row = 0; row < SIZE_TABLE * 2 - 1; row++) {
+            for (let col = 0; col < arr3[row].length; col++) {
+
+                if (arr3[row].length >= SEQUENCE_WIN) {
+
+                    temp3 = arr3[row][col];
+                    temp4 = arr4[row][col];
+
+                    if (temp3.value == VALUE_0) {
+                        sequenceArr30.push(arr3[row][col]);
+                        sequenceArr3X = [];
+                    } else if (temp3.value == VALUE_X) {
+                        sequenceArr3X.push(arr3[row][col]);
+                        sequenceArr30 = [];
+                    } else {
+                        sequenceArr3X = [];
+                        sequenceArr30 = [];
+                    }
+    
+                    if (temp4.value == VALUE_0) {
+                        sequenceArr40.push(arr4[row][col]);
+                        sequenceArr4X = [];
+                    } else if (temp4.value == VALUE_X) {
+                        sequenceArr4X.push(arr4[row][col]);
+                        sequenceArr40 = [];
+                    } else {
+                        sequenceArr4X = [];
+                        sequenceArr40 = [];
+                    }
+
+                    if (sequenceArr30.length == SEQUENCE_WIN) {
+                        return sequenceArr30;
+                    }
+                    if (sequenceArr3X.length == SEQUENCE_WIN) {
+                        return sequenceArr3X;
+                    }
+                    if (sequenceArr40.length == SEQUENCE_WIN) {
+                        return sequenceArr40;
+                    }
+                    if (sequenceArr4X.length == SEQUENCE_WIN) {
+                        return sequenceArr4X;
+                    }
+                }
+            }
+
+            sequenceArr3X = [];
+            sequenceArr30 = [];
+            sequenceArr4X = [];
+            sequenceArr40 = [];
+        }
+        return null;
+    } 
+
+    // Функция проверки есть ли на поле SEQUENCE_WIN в ряд либо X, либо 0
+    function isContainSequnceInRow() {
+
         let arr1 = [];  // -> указание направления, которым записан массив, начало слева и вверх
         let arr2 = [];  // \/
         let arr3 = [];  // /
         let arr4 = [];  // \
 
-        let rowArr = 0;
-        let colArr = 0;
-
         let valueX0;
+        let temp = [];
 
         $(".wrap-playing-field-table tr td").each(function (index, val) {
-            if (index % 11 != 0) {
-                let rightCoord = Math.floor(index / 11);
-                let topCoord = (index - rightCoord) - (rightCoord * 10) - 1;
-                
+            if ((index % (SIZE_TABLE + 1)) != 0) {
+                let rightCoord = Math.floor(index / (SIZE_TABLE + 1));
+                let topCoord = (index - rightCoord) - (rightCoord * SIZE_TABLE) - 1;
+
                 if ($(val).html() == "") valueX0 = 0;
                 if ($(val).html() == "0") valueX0 = 0.5;
                 if ($(val).html() == "X") valueX0 = 1;
 
-                arr1[10 * rightCoord + topCoord] = valueX0;
-                arr2[10 * topCoord + rightCoord] = valueX0;
-                arr3[10 * rowArr + colArr] = valueX0;
-                
-                
+                temp.push({ 
+                    value: valueX0, 
+                    coord: [ rightCoord, topCoord ]
+                });
+                if (topCoord == 9) {
+                    arr1[rightCoord] = temp;
+                    temp = [];
+                }
+                if (rightCoord == 0) {
+                    arr2[topCoord] = [];
+                }
+                arr2[topCoord].push({ 
+                    value: valueX0, 
+                    coord: [ rightCoord, topCoord ]
+                });              
             }
         });
-        /*
-         int rowArr = 0;
-            int colArr = 0;
-            int pob = 0;
-            int index = 0;
-            int[] newArr = new int[25];
-            while (rowArr != 5 && colArr != 5)
-            {
-                newArr[index] = arr[rowArr - pob, colArr + pob];
-                if (rowArr - pob == 0 || colArr + pob == 4)
-                {
-                    if (rowArr < 4)
-                    {
-                        rowArr++;
-                    }
-                    else
-                    {
-                        colArr++;
-                    }
-                    pob = 0;
-                }
-                else
-                {
-                    pob++;
-                }
-                index++;
-            }
-        */
 
-        return false;
+        //формирование диагонального массива слева, вверх на право 
+        let rowArr = 0; 
+        let colArr = 0;
+        let pob = 0;
+        let index = 0;
+
+        while (rowArr != SIZE_TABLE && colArr != SIZE_TABLE) {
+            if (pob == 0) { 
+                arr3[index] = [];
+            }
+            arr3[index].push(arr1[rowArr - pob][colArr + pob]);
+            if (rowArr - pob == 0 || colArr + pob == SIZE_TABLE - 1) {
+                if (rowArr < SIZE_TABLE - 1) {
+                    rowArr++;
+                } else {
+                    colArr++;
+                }
+                pob = 0;
+                index++;
+            } else {
+                pob++;
+            }
+        }
+
+        //формирование диагонального массива слева, вверх на право 
+        rowArr = 0;
+        colArr = SIZE_TABLE - 1;
+        pob = 0;
+        index = 0;
+        arr4[index] = [];
+
+        while (rowArr != SIZE_TABLE && colArr != -1) {
+            if (pob == 0) { 
+                arr4[index] = [];
+            }
+            arr4[index].push(arr1[rowArr - pob][colArr - pob]);
+            if (rowArr - pob == 0 || colArr - pob == 0) {
+                if (rowArr != SIZE_TABLE - 1) {
+                    rowArr++;
+                } else {
+                    colArr--;
+                }
+                pob = 0; 
+                index++;
+            } else {
+                pob++;
+            }
+        }
+
+        let lineirResult = isContainSequenceLineir(arr1, arr2);
+
+        if (lineirResult != null) {
+           return lineirResult;
+        } else {
+
+            let diagonalResult = isContainSequnceDiagonal(arr3, arr4);
+
+            if (diagonalResult != null) {
+                return diagonalResult;
+            }
+        }
+
+        return null;
     }
 
     // Навешивание обработчика события на ячейку игрового поля
     $(".wrap-playing-field-table tr td").each(function (index, val) {
-        if (index % 11 != 0) {
-            $(val).on("click", function(e) {
-                if (!isContainFiveInRow()) {
-                    $(this).html((isX) ? ("X") : ("0"));
-                    makeMove(getSendObject(getArrayCoefficients(), index), getArrayCoefficients());
-                }
+        if (index % (SIZE_TABLE + 1) != 0) {
+            $(val).on("click", function() {
+
+                $(this).html((IS_X) ? ("X") : ("0"));
+
+                let result = isContainSequnceInRow();
+                if (result != null) {
+                    endGame(result);
+                } else {
+                    $(this).html("");
+                    makeMove(getTrainData(getArrayCoefficients(), index), getArrayCoefficients());
+                }  
+
+                $(this).html((IS_X) ? ("X") : ("0"));
+                $(this).unbind("click");
             });
         }
+    });
+
+    $(".wrap-info-btn-end-game").click(function() {
+        showLoading("Start a new game");
+        location.reload();
     });
 });
